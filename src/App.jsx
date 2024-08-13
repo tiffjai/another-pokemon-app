e5bdda9cd6fa1d7bb8fcccd87d647c600e2a8eef
Stephanie <tiffanieho@outlook.com>
2024-08-13 15:53:05 +0100

    change color to css

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
- 
-import React, { useState, useEffect } from 'react';
+ import React, { useState, useEffect } from 'react';
 
 const App = () => {
   const [pokemonName, setPokemonName] = useState('');
